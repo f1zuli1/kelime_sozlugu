187c52f95c02f343b33d8de42903cec9063cf796
f1zuli1 <fuzuliisr06@gmail.com>
2024-08-28 23:08:28 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+modern_sozluk = {
+                "CRINGE": "garip ya da utandirici bir sey",
+                "LOL": "komik bir seye verilen cevap",
+                "BOOMER": "yasli eski jenarasyon",
+                "AGGRO": "agresiflesmek",
+                "SHEESH": "onaylamak",
+                "TOKSIK": "kendi başarısızlıklarını ve kendi mutsuzluklarını karşısındaki kişiyi ayna olarak görüp yansıtan kişidir",
+                "CILGIN": "aşırı heyecanlı, deli, akıl almaz",
+                "KOKOFONI": "Uyduruk, hoş olmayan seslerin karışımı",
+                "kakofoni": "Uyduruk, hoş olmayan seslerin karışımı",
+                "çılgın": "Aşırı heyecanlı, deli, akıl almaz",
+                "toksik": "kendi başarısızlıklarını ve kendi mutsuzluklarını karşısındaki kişiyi ayna olarak görüp yansıtan kişidir",
+}
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+if word in modern_sozluk.keys():
+       print(modern_sozluk[word])
